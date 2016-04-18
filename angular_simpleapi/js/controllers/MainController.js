52a7cc7ceea435d762532d6ed6c3f5dee19a417e
dhirajpatra <dhiraj.patra@gmail.com>
2016-04-18 20:56:38 +0530

    added angularjs to work with node

---
+++ angular_simpleapi/js/controllers/MainController.js
@@ -0,0 +1,43 @@
+app.controller('MainController', ['$scope','$http', 
+
+function($scope, $http) {
+    var result;
+    
+    // common http API related directives
+    $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    //$http.defaults.headers.post["X-API-KEY"] = "41CB86796F3BA";
+    $http.crossDomain = true; 
+    $scope.url = "http://localhost:8080/";
+    
+    // for fetchallusers API
+    $scope.fetchallusers = function(){
+        //console.log("i am here ");        
+        result = $http.get($scope.url + "users").success(function(data, status){
+            result = (data);
+            console.log(result);
+        }).error(function(data, status){
+            $scope.status = status;
+            console.log(status);
+        });
+        return result;
+        
+    }
+
+    // for createuser API
+    $scope.createuser = function(){
+        console.log("i am here " + $scope.email);
+        /*$http({
+                method: "post",
+                url: $scope.url + "users",
+                data: "{\"email\":\""+$scope.email+"\",\"password\":\""+$scope.password+"\",\"phone\":\""+$scope.phone+"\",\"address1\":\""+$scope.address1+"\",\"address2\":\""+$scope.address2+"\",\"pin\":\""+$scope.pin+"\,\"country\":\""+$scope.country+"\"}"
+        }).success (function(data,status,headers,config){                     
+             $scope.data = data;
+             console.log(data);
+        }).error(function(data, status, headers, config){
+             $scope.status = status;
+            console.log(status);
+        });*/
+        
+    }
+		
+}]);
--- server.js
+++ server.js
@@ -4,6 +4,17 @@ var bodyParser  =   require("body-parser");
 var userModel    =   require("./models/user");
 var router      =   express.Router();
 
+var http = require('http');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain);
+
 // user schema
 var userDB  = userModel.User;
 // address schema
